02bcb0f05d54e32fd07d2914810ba8cc53e27c2e
reggi <thomas@reggi.com>
2013-10-07 18:37:26 -0400

    index out & readme

--- fontcustom.sh
+++ fontcustom.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 fontcustom compile ./svg --no-hash
 cp ./dist/font-holstee.css ./dist/font-holstee.less
-perl -pi -w -e 's/\.\/dist/\@\{fontHolsteePath\}\/dist/g' ./dist/font-holstee.less
+perl -pi -w -e 's/\(\"\.\/font-holstee/\(\"\@\{fontHolsteePath\}/g' ./dist/font-holstee.less
+mv ./dist/font-holstee-preview.html ./index.html
+perl -pi -w -e 's/\(\"\.\/font-holstee/\(\"\.\/dist\/font-holstee/g' ./index.html
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Font Holstee
+
+Icons designed by [@andrewmartis](http://twitter.com/andrewmartis)
+Compiled with [fontcustom](https://github.com/FontCustom/fontcustom)
